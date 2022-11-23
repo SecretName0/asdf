ff09a5d8a21fc8c71fb32ee407355abf9b8017b5
SecretName0 <117903998+SecretName0@users.noreply.github.com>
2022-11-23 19:46:32 +0900

    z
    2022.11.23

--- Project1/소스2.c
+++ Project1/소스2.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 int main(void)
 {
-	int data = 1;
-	float dd = 1.2;
 
-	switch (dd)  // 스위치 문에는 조건식으로 실수형 변수와 실수형 상수를 사용할 수 없음
-	{
-	case 0: printf("Title \n");
-		break;  
-	case 1: printf("Village \n");
-		break;
-	case 2: printf("Dungeon \n");
-		break;
-	default: printf("개같이 멸망 \n");
-		break;
-	case 3: printf("dd");
-    }
 }
-		
