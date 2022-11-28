88776032db887f1fd6941d871c7a7abdd8ca2072
SecretName0 <117903998+SecretName0@users.noreply.github.com>
2022-11-28 18:00:44 +0900

    Update 소스2.c

--- Project1/소스2.c
+++ Project1/소스2.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-
+	for (int z = 2; z <= 9; z++)
+	{
+		printf("%d´Ü \n", z);
+		for (int x = 2; x <= 9; x++)
+		{
+			for (int y = 1; y <= 9; y++)
+			{
+				printf("%d x %d = %d \n", x, y, x * y);
+			}
+			printf("\n");
+		}
+	}
 }
