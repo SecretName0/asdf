8373e441ab089cd7f2c613c3cd44cfc86767a2ab
SecretName0 <117903998+SecretName0@users.noreply.github.com>
2022-11-28 19:46:14 +0900

    모듈러 연산자
    2022 11 28

--- Project1/소스2.c
+++ Project1/소스2.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 int main(void)
 {
-	for (int z = 2; z <= 9; z++)
-	{
-		printf("%d´Ü \n", z);
-		for (int x = 2; x <= 9; x++)
-		{
-			for (int y = 1; y <= 9; y++)
-			{
-				printf("%d x %d = %d \n", x, y, x * y);
-			}
-			printf("\n");
-		}
-	}
+
 }
