62c9332a3f603bfbe20e29b97ef1e6dc5b53dda2
SecretName0 <117903998+SecretName0@users.noreply.github.com>
2022-12-13 00:45:10 +0900

    포인터 연산
    2022

---
+++ Project1/014 포인터 연산.h
@@ -0,0 +1,23 @@
+// 포인터 연산
+/*
+int data[5] = { 1,2,3,4,5 };
+
+int* ptr = data;
+
+printf("ptr이 가라키는 값 : %d\n", *ptr);
+
+ptr = ptr + 2;
+
+printf("ptr을 증가시킨 후 가리키는 값 : %d\n", *ptr);
+
+// 1
+// 홀수
+ptr = ptr - 1;
+
+// 10
+// 짝수
+printf("ptr을 감소시킨 후 가리키는 값 : %d\n", *ptr);
+
+return 0;
+}
+*/
